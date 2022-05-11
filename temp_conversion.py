b4a904bf0036fb0232cb18052a0a168badb6eb02
AlluKoodaa <104388003+AlluKoodaa@users.noreply.github.com>
2022-05-11 08:40:25 +0300

    Add files via upload

---
+++ temp_conversion.py
@@ -0,0 +1,66 @@
+#--AlluKoodaa--#
+
+import temp_conversion_module as converter
+
+
+def menu():
+    print("""Choose conversion:
+1) Celsius->Fahrenheit
+2) Celsius->Kelvin
+3) Fahrenheit->Kelvin
+4) Fahrenheit->Celsius
+5) Kelvin->Celsius
+6) Kelvin->Fahrenheit
+0) Quit""")
+
+    choice = input("Your choice: ")
+    try:
+        if int(choice) % 1 != 0:
+            return None
+    except ValueError:
+        return None
+    else:
+        return int(choice)
+
+def main():
+    print(f"Using temperature conversion module, version: {converter.VERSION}")
+
+    while True:
+        choice = menu()
+        if choice == 0:
+            break
+        elif choice == None:
+            print("\nInvalid input. Please input number.\n")
+            continue
+        if choice == 1:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ctf(temperature)
+            print(f"{temperature}C in Fahrenheit:", round(tulos, 2))
+        elif choice == 2:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ctk(temperature)
+            print(f"{temperature}C in Kelvin:", round(tulos, 2))
+        elif choice == 3:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ftk(temperature)
+            print(f"{temperature}F in Kelvin:", round(tulos, 2))
+        elif choice == 4:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ftc(temperature)
+            print(f"{temperature}F in Celcius:", round(tulos, 2))
+        elif choice == 5:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ktc(temperature)
+            print(f"{temperature}K in Celsius:", round(tulos, 2))
+        elif choice == 6:
+            temperature = float(input("Input temperature: "))
+            tulos = converter.ktf(temperature)
+            print(f"{temperature}K in Fahrenheit:", round(tulos, 2))
+        else:
+            print("\nInvalid input. Try again.")
+        print()
+    print("\nThanks for using my converter. Peace out!")
+
+main()
+
+#--eof--#
---
+++ temp_conversion_module.py
@@ -0,0 +1,23 @@
+#--AlluKoodaa--#
+
+def ftc(x): # Fahrenheit to Celsius
+    return (x -32) * (5 / 9)
+
+def ktc(x): # Kelvin to Celsius
+    return x - 273.15
+
+def ftk(x): # Fahrenheit to Kelvin
+    return (x + 459.67) * (5 / 9)
+
+def ctk(x): # Celsius to Kelvin
+    return x + 273.15
+
+def ktf(x): # Kelvin to Fahrenheit
+    return x * (9 / 5) - 459.67
+
+def ctf(x): # Celsius to Fahrenheit
+    return x * (9 / 5) + 32
+
+VERSION = 1.0
+
+#--eof--#
